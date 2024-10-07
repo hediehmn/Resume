6b2bbc0215a21c3e840841d071b35f5eb6b8f0a5
hedieh <hedieh.mni@gmail.com>
2024-10-07 23:57:35 +0330

    Resume

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      Vazirmatn:["Vazirmatn"],
+      ABSTER:["ABSTER"],
+      DanaFaNum:["DanaFaNum"],
+    },
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg':'992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+    },
+  
+  },
+  plugins: [],
+}
+
